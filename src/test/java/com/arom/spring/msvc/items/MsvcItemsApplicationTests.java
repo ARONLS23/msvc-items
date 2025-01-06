3e5f684fb0d645a9873bc466d4b6b158109bf1fa
ARONLS23 <aronsls20@gmail.com>
2025-01-06 10:52:46 -0500

    proyecto base

---
+++ src/test/java/com/arom/spring/msvc/items/MsvcItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arom.spring.msvc.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
