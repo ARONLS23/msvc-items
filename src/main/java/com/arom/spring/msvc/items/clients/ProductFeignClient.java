b785952d7897fd73b32b45f50b46e8bfc751c248
ARONLS23 <aronsls20@gmail.com>
2025-01-09 15:02:19 -0500

    feat add balanceo de carga

--- src/main/java/com/arom/spring/msvc/items/clients/ProductFeignClient.java
+++ src/main/java/com/arom/spring/msvc/items/clients/ProductFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-@FeignClient(url = "localhost:8001/api/products", name = "msvc-products")
+@FeignClient(name = "msvc-products")
 public interface ProductFeignClient {
 
     @GetMapping
